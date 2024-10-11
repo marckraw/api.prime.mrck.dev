f9e7ff177d702fd6f757ee643d15e61342a9f740
Marcin Krawczyk <marckraw@icloud.com>
2024-10-12 00:10:03 +0200

    add test endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/test', (c) => {
+  return c.json({ message: 'Message man!. Hello Hono!' })
+})
+
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
