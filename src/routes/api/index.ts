1af4a4d552deafcd832d97c9557a1bb4e1facb25
Marcin Krawczyk <marckraw@icloud.com>
2024-10-18 15:44:09 +0200

    fix: add some cors basic boilerplate

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,8 +2,15 @@ import {Hono} from "hono";
 import { bearerAuth } from 'hono/bearer-auth'
 import chatRouter from "../chat/chat";
 import {config} from "../../config.env";
+import { cors } from 'hono/cors'
 
 const apiRouter = new Hono()
+apiRouter.use(
+    '/*',
+    cors({
+        origin: ['https://example.com', 'https://example.org'],
+    })
+)
 
 const token = config.X_API_KEY
 
