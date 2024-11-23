631ad9237740945be88f3aff0596eb56d98d8af0
Marcin Krawczyk <marckraw@icloud.com>
2024-11-23 11:46:44 +0100

    add langfuse test to intention-validation

--- src/services/IntentionValidator/IntentionValidator.ts
+++ src/services/IntentionValidator/IntentionValidator.ts
@@ -1,6 +1,7 @@
 import { AntonSDK } from "@mrck-labs/anton-sdk";
 import { config } from "../../config.env";
 import { intentionValidationPrompt } from "../../routes/api/agi/prompts/intentionValidationPrompts";
+import { Langfuse } from "langfuse";
 
 export class IntentionValidator {
     intentionValidator: any; 
@@ -15,9 +16,25 @@ export class IntentionValidator {
     }
 
     async validateAndClassify({messages}: {messages: any[]}) {
+        const langfuse = new Langfuse();
+        const trace = langfuse.trace({
+            name: "agi-intention-validation",
+        });
+
+        const generation = trace.generation({
+            name: "chat-completion",
+            model: "gpt-4o",
+            input: messages,
+          });
+
         const response = await this.intentionValidator.chat({
             messages: messages,
         })
+
+        // End generation - sets endTime
+        generation.end({
+            output: response,
+        });
 
         return response
     }
